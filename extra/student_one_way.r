6418bdb5451b97ca278cd01ad473ad930baa7416
var71 <vedaksuyog7@gmail.com>
2026-03-13 20:34:45 +0530

    Update student_one_way.r

--- extra/student_one_way.r
+++ extra/student_one_way.r
@@ -1,10 +1,5 @@
-classA <- c(85,90,88,82,87)
-classB <- c(76,78,80,81,75)
-classC <- c(92,88,94,89,90)
+scores <- read.csv(file.choose(), header=TRUE)
 
-scores <- c(classA, classB, classC)
-group <- factor(rep(c("A","B","C"), each=5))
-
-anova_result <- aov(scores ~ group)
+anova_result <- aov(score ~ class, data=scores)
 
 summary(anova_result)
