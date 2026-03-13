d5da291ff6f516fc75b7dd6daaba2b9b8a831a26
var71 <vedaksuyog7@gmail.com>
2026-03-13 20:35:55 +0530

    Update student_one_way.r

--- extra/student_one_way.r
+++ extra/student_one_way.r
@@ -3,3 +3,9 @@ scores <- read.csv(file.choose(), header=TRUE)
 anova_result <- aov(score ~ class, data=scores)
 
 summary(anova_result)
+
+
+dataset=# Scores of each class
+class_A = [85, 90, 88, 82, 87]
+class_B = [76, 78, 80, 81, 75]
+class_C = [92, 88, 94, 89, 90]
